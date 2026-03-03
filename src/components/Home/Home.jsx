212bda46aefc5d0cbe225e8ce3d161c264a1c318
menabil <nabil99cc@gmail.com>
2026-03-03 18:50:15 +0600

    add project section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import About from "./About";
 import Banner from "./Banner";
+import Project from "./Project";
 import Service from "./Service";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner />
       <About />
       <Service />
+      <Project />
     </>
   );
 };
---
+++ src/components/Home/Project.jsx
@@ -0,0 +1,13 @@
+import Container from "../common/Container"
+
+const Project = () => {
+    return (
+        <div>
+            <Container>
+                <h3 className="text-[45px] text-[#1B1B1B] font-semibold leading-[54px] capitalize">our latest project</h3>
+            </Container>
+        </div>
+    )
+}
+
+export default Project
