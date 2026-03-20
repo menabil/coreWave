ebd7631add28d9b6b4dfea19f884bf5e506edccd
meNabil <nabil99cc@gmail.com>
2026-03-20 10:56:33 +0600

    create review page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import About from "./About";
 import Banner from "./Banner";
+import Review from "./Review";
 import Service from "./Service";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner />
       <About />
       <Service />
+      <Review />
     </>
   );
 };
---
+++ src/components/Home/Review.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "../common/Container";
+
+const Review = () => {
+  return (
+    <div className="py-25">
+      <Container>
+        <h3 className="text-center text-[#1B1B1B] text-[45px] font-semibold leading-[54px]">What our clients saying</h3>
+      </Container>
+    </div>
+  );
+};
+
+export default Review;
