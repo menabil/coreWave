bb9849a244a8069f18d40f1853e35ec3832be4bb
menabil <nabil99cc@gmail.com>
2026-04-28 00:53:59 +0600

    responsive service section

--- src/components/Home/Service.jsx
+++ src/components/Home/Service.jsx
@@ -3,56 +3,58 @@ import Flex from "../common/Flex";
 
 const Service = () => {
   return (
-    <div className="py-[70px] bg-[#EEFFF9] rounded-tl-[20%] rounded-br-[20%]">
+    <div className="lg:py-[70px] py-10 bg-[#EEFFF9] lg:rounded-tl-[20%] lg:rounded-br-[20%]">
       <Container>
-        <Flex className={"justify-between"}>
-          <div className="flex flex-wrap gap-10">
-            <div className="pt-12 pb-[60px] px-12 rounded-[10px] bg-white shadow-newMade">
+        <Flex
+          className={"lg:justify-between flex-col-reverse lg:flex-row"}
+        >
+          <div className="flex flex-wrap lg:gap-10 gap-5 justify-center">
+            <div className="lg:pt-12 lg:pb-[60px] lg:px-12 px-7 pt-7 pb-10 rounded-[10px] bg-white shadow-newMade">
               <img src="/src/assets/aOne.png" alt="" />
               <h6 className="text-xl font-semibold py-3.5">Web Development</h6>
-              <p className="text-base text-[#7B7B7B] leading-[26px] w-[217px] ">
+              <p className="lg:text-base text-sm text-[#7B7B7B] leading-[26px] w-[217px] ">
                 Web development is the art of creating engaging and visually
                 appealing websites
               </p>
             </div>
-            <div className="pt-12 pb-[60px] px-12 rounded-[10px] bg-[#06C279]/50 shadow-newMade">
+            <div className="lg:pt-12 lg:pb-[60px] lg:px-12 px-7 pt-7 pb-10 rounded-[10px] bg-[#06C279]/50 shadow-newMade">
               <img src="/src/assets/aTwo.png" alt="" />
               <h6 className="text-xl font-semibold text-white py-3.5">
                 Web Development
               </h6>
-              <p className="text-base text-white leading-[26px] w-[217px]">
+              <p className="lg:text-base text-sm text-white leading-[26px] w-[217px]">
                 Web development is the art of creating engaging and visually
                 appealing websites
               </p>
             </div>
-            <div className="pt-12 pb-[60px] px-12 rounded-[10px] bg-white shadow-newMade">
+            <div className="lg:pt-12 lg:pb-[60px] lg:px-12 px-7 pt-7 pb-10 rounded-[10px] bg-white shadow-newMade">
               <img src="/src/assets/aThr.png" alt="" />
               <h6 className="text-xl font-semibold py-3.5">Web Development</h6>
-              <p className="text-base text-[#7B7B7B] leading-[26px] w-[217px] ">
+              <p className="lg:text-base text-sm text-[#7B7B7B] leading-[26px] w-[217px] ">
                 Web development is the art of creating engaging and visually
                 appealing websites
               </p>
             </div>
-            <div className="pt-12 pb-[60px] px-12 rounded-[10px] bg-white  shadow-newMade">
+            <div className="lg:pt-12 lg:pb-[60px] lg:px-12 px-7 pt-7 pb-10 rounded-[10px] bg-white  shadow-newMade">
               <img src="/src/assets/aFor.png" alt="" />
               <h6 className="text-xl font-semibold py-3.5">Web Development</h6>
-              <p className="text-base text-[#7B7B7B] leading-[26px] w-[217px]">
+              <p className="lg:text-base text-sm text-[#7B7B7B] leading-[26px] w-[217px]">
                 Web development is the art of creating engaging and visually
                 appealing websites
               </p>
             </div>
           </div>
-          <div className="">
-            <h3 className="text-[45px] font-semibold leading-[54px] w-[437px]">
+          <div className="mb-10">
+            <h3 className="font-semibold lg:text-[45px] text-3xl lg:w-[437px] w-[280px] lg:leading-[54px] leading-10">
               We offer a variety of services such as
             </h3>
-            <p className="text-[#7B7B7B] leading-[26px] text-base w-[543px] py-[30px]">
+            <p className="text-[#7B7B7B] py-[30px] lg:text-base text-sm leading-[26px] lg:w-[543px] w-[280px] pt-4 pb-6">
               At our company, we pride ourselves on offering a variety of
               services to meet the diverse needs of our clients. Whether you're
               looking for marketing assistance, website design, or IT support,
               we've got you covered
             </p>
-            <button className="rounded-[5px] bg-[#DAF6EB] py-3.5 px-6 text-[#06C279] font-semibold cursor-pointer hover:bg-transparent border-3 border-[#DAF6EB] duration-300">
+            <button className="rounded-[5px] bg-[#DAF6EB] lg:py-3.5 lg:px-6 px-3 py-2 text-[#06C279] font-semibold cursor-pointer hover:bg-transparent border-3 border-[#DAF6EB] duration-300 lg:text-base text-sm">
               All Services
             </button>
           </div>
