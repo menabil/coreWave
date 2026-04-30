893e092bcd7636c779e9de40e9d79171e03ef296
menabil <nabil99cc@gmail.com>
2026-04-30 20:12:27 +0600

    responsive trail section

--- src/components/Home/Trail.jsx
+++ src/components/Home/Trail.jsx
@@ -4,13 +4,20 @@ import tOne from "/src/assets/trail.png";
 
 const Trail = () => {
   return (
-    <div className="bg-[#06c179] py-[86px] relative">
+    <div className="bg-[#06c179] lg:py-[86px] py-10 relative">
       <Container>
-        <h3 className="text-[45px] text-white font-semibold leading-[54px] mb-9.5">
+        <h3 className="lg:text-[45px] text-3xl text-white font-semibold lg:leading-[54px] leading-10 mb-9.5">
           Don’t worries, start your free trial today!
         </h3>
-        <Images imgSrc={tOne} className={"absolute -bottom-3 right-30"} />
-        <button className={"font-semibold bg-[#2F2F2F]! border-none"}>
+        <Images
+          imgSrc={tOne}
+          className={"absolute -bottom-3 right-30 hidden lg:block"}
+        />
+        <button
+          className={
+            "font-semibold bg-[#2F2F2F] lg:text-base text-sm border-none py-3.5 px-6 rounded-[5px] text-white cursor-pointer"
+          }
+        >
           Get Free Trial
         </button>
       </Container>
--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -10,13 +10,21 @@ import { FaBehance } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="mt-25">
+    <div className="lg:mt-25 mt-10">
       <Container>
-        <Flex className={"items-start gap-x-[195px] mb-10.5"}>
-          <div className="">
+        <Flex
+          className={
+            "items-start gap-x-[195px] mb-10.5 flex-col-reverse lg:flex-row"
+          }
+        >
+          <div className="mx-auto">
             <Images imgSrc={fLogo} />
-            <p className="text-sm text-[#7B7B7B] leading-5.5 mt-6.5">mukimsdesign@gmail.com</p>
-            <p className="text-sm text-[#7B7B7B] leading-5.5 mb-5">+88 01767630044</p>
+            <p className="text-sm text-[#7B7B7B] leading-5.5 mt-6.5">
+              mukimsdesign@gmail.com
+            </p>
+            <p className="text-sm text-[#7B7B7B] leading-5.5 mb-5">
+              +88 01767630044
+            </p>
             <Flex className={"gap-x-2"}>
               <div className="rounded-full bg-[#06C279] h-7 w-7 text-white relative cursor-pointer">
                 <FaFacebookF className="absulate top-1/2 left-1/2  translate-x-1/3 translate-y-[40%]" />
@@ -32,9 +40,13 @@ const Footer = () => {
               </div>
             </Flex>
           </div>
-          <div className="">
-            <h6 className="text-xl text-[#1B1B1B] mb-6.5 leading-7.5 font-semibold">Services</h6>
-            <Flex className={"flex-col text-sm text-[#7B7B7B] gap-y-2 items-start"}>
+          <div className="hidden md:block">
+            <h6 className="text-xl text-[#1B1B1B] mb-6.5 leading-7.5 font-semibold">
+              Services
+            </h6>
+            <Flex
+              className={"flex-col text-sm text-[#7B7B7B] gap-y-2 items-start"}
+            >
               <p>Web Design</p>
               <p>Web Development</p>
               <p>SEO Marketing</p>
@@ -42,22 +54,35 @@ const Footer = () => {
               <p>Digital Marketing</p>
             </Flex>
           </div>
-          <div className="">
-            <h6 className="text-xl text-[#1B1B1B] mb-6.5 leading-7.5 font-semibold">Help</h6>
-            <Flex className={"flex-col text-sm text-[#7B7B7B] gap-y-2 items-start"}>
+          <div className="hidden md:block">
+            <h6 className="text-xl text-[#1B1B1B] mb-6.5 leading-7.5 font-semibold">
+              Help
+            </h6>
+            <Flex
+              className={"flex-col text-sm text-[#7B7B7B] gap-y-2 items-start"}
+            >
               <p>Account</p>
               <p>Support Center</p>
               <p>Privacy Policy</p>
               <p>Terms & Conditions</p>
             </Flex>
           </div>
-          <div className="">
-            <h6 className="text-xl text-[#1B1B1B] mb-6.5 leading-7.5 font-semibold">Contact Us</h6>
-            <input type="text" />
+          <div className="lg:pb-0 pb-10 lg:text-left text-center">
+            <h6 className="text-xl text-[#1B1B1B] leading-7.5 font-semibold">
+              Contact Us
+            </h6>
+            <input
+              type="email"
+              placeholder="Enter your mail"
+              className="px-4 py-3 lg:w-[337px] w-[270px] border-2 border-[#DDDDDD] rounded-[5px] lg:mt-7.5 my-3 lg:mb-3.5 outline-none placeholder:text-[#BDBDBD]"
+            />
+            <button className="rounded-[5px] bg-[#06C279] lg:py-3.5 lg:px-6 px-3 py-2 text-white font-semibold cursor-pointer hover:bg-[#DAF6EB] duration-300 lg:text-base text-sm hover:text-[#06C279]">
+              Request & Callback
+            </button>
             {/* <Button btnText={"Request & Callback"} /> */}
           </div>
         </Flex>
-        <div className="border-t-2 border-[#E0E0E0] text-center w-full text-sm font-medium text-[#7B7B7B] leading-5.5 py-4">
+        <div className="border-t-2 border-[#E0E0E0] text-center w-full lg:text-sm text-xs font-medium text-[#7B7B7B] leading-5.5 py-4">
           @2023 CoreWave. All copyrights reserved
         </div>
       </Container>
